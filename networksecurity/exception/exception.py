6d78f4112cdeddf29307e3c5444045956087264c
Var-TerminalWizard <bestavarunsai@gmail.com>
2026-06-05 22:33:21 +0530

    Data Ingestion component

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,22 +1,32 @@
 import sys
-from networksecurity.logging import logger
+from networksecurity.logging.logger import logging
+
 
 class NetworkSecurityException(Exception):
-    def __init__(self,message,details:sys):
-        self.message = message
-        _, _, tb = details.exc_info()
-        
-        self.lineno = tb.tb_lineno
-        self.filename = tb.tb_frame.f_code.co_filename
-    
-    def __str__(self):
-        self.filename, self.lineno, str(self.error_message)
-        return f"Error occurred in file: {self.filename} at line: {self.lineno} with message: {self.message}"
+    def __init__(self, error_message: Exception, details=sys):
+        super().__init__(str(error_message))
+        self.error_message = error_message
+        self.filename = None
+        self.lineno = None
+        try:
+            exc_type, exc_value, tb = details.exc_info()
+            if tb is not None:
+                self.lineno = tb.tb_lineno
+                self.filename = tb.tb_frame.f_code.co_filename
+        except Exception:
+            # fail-safe: keep filename/lineno as None
+            pass
+
+    def __str__(self) -> str:
+        return (
+            f"Error occurred in file: {self.filename} at line: {self.lineno} with message: {self.error_message}"
+        )
+
 
 if __name__ == "__main__":
     try:
-        logger.logging.info("This is a test log message.")
-        a=1/0
-        print("This will not be printed",a)
+        logging.info("This is a test log message.")
+        a = 1 / 0
+        print("This will not be printed", a)
     except Exception as e:
-        raise NetworkSecurityException(e,sys)
+        raise NetworkSecurityException(e, sys)
