7292ef929aee5be5211a7dda44a22654c90c2928
Var-TerminalWizard <bestavarunsai@gmail.com>
2026-06-17 17:52:05 +0530

    Data Transfromation is completed.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation 
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+
+
 
 import sys
 
@@ -35,5 +38,14 @@ if __name__=='__main__':
         data_validation_artifact = data_validation.initialize_data_validation()
         logging.info(f"Data validation completed")
         print(data_validation_artifact)
+        logging.info(f"Data transformation started")
+        data_transformation_config = DataTransformationConfig(training_pipeline_config)
+        data_transformation = DataTransformation(
+            data_validation_artifact=data_validation_artifact,
+            data_transformation_config=data_transformation_config
+        )
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info(f"Data transformation completed")
     except Exception as e:
             raise NetworkSecurityException(e,sys)
--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -25,3 +25,13 @@ DATA_VALIDATION_VALID_DIR = "validated"
 DATA_VALIDATION_INVALID_DIR = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+
+DATA_TRANSFORMATION_DIR_NAME = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DIR = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR = "transformed_object"
+DATA_TRANSFORMATION_IMPUTER_PARAMS = {
+    "missing_values" : np.nan,
+    "n_neighbors" : 3,
+    "weights" : "uniform",
+}
